faa5615a4f46653423c3b7325a1325dabb4d963e
Feras Jobeir <fjobeir@gmail.com>
2023-01-06 11:10:13 +0300

    Category Validation

--- routes/categories.js
+++ routes/categories.js
@@ -1,8 +1,14 @@
 var express = require("express");
-const {store, index, update, destroy, show} = require("../controllers/categoryController");
+const { store, index, update, destroy, show } = require("../controllers/categoryController");
 var router = express.Router();
+const { body } = require('express-validator');
 
-router.post("/", store);
+
+router.post("/",
+    body('name').isLength({ min: 2, max: 20 }),
+    body('description').isLength({ min: 2, max: 20 }),
+    store
+);
 router.get("/", index);
 router.put("/:id", update);
 router.delete("/:id", destroy);
